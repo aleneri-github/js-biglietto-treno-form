f3cc1e2f9c29453702e9c65d95a076066857d0bb
Alessandro <aleneri242@gmail.com>
2020-11-14 16:02:20 +0100

    commit 8 sabato

--- js/script.js
+++ js/script.js
@@ -10,57 +10,47 @@ function() {
 
   var nomeCognome = document.getElementById("nome_cognome").value;
   console.log(nomeCognome);
+
+
+  // NOTE: PREZZO AL KM
+  var prezzoAlKm = 0.21;
+  // // NOTE: KM INSERITI DALL'UTENTE
+  // var richiestaKm = prompt("Inserisci i km che vuoi percorrere");
+  // ETA' INSERITA DALL'UTENTE
+  // var anni = prompt("Inserisci la tua età");
+  // CALCOLO PREZZO INTERO
+  var prezzoIntero = numeroKm * prezzoAlKm;
+  console.log(prezzoIntero);
+  // NOTE: ARROTONDAMENTO PREZZO
+  var prezzoArrotondato = prezzoIntero.toFixed(2);
+  // VARIABILE SCONTO
+  var sconto = 0;
+  // SCONTO UNDER18
+  if (fasciaEta == "Minorenne") {
+    sconto = (prezzoIntero * 20 ) / 100;
+  // SCONTO OVER65
+} else if (fasciaEta == "Over65") {
+    sconto = (prezzoIntero * 40) / 100;
+  }
+  // PREZZO SCONTATO
+  var prezzoFinale = prezzoIntero - sconto;
+  console.log(prezzoFinale);
+
+  // document.getElementById("km").innerHTML = richiestaKm + " Km";
+  //
+  // document.getElementById("anni").innerHTML = anni + " Anni";
+  //
+  // document.getElementById("totale").innerHTML = prezzoArrotondato + " €";
+
+
+
+
+
+
+
+
+
+
+
   }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // NOTE: PREZZO AL KM
-// var prezzoAlKm = 0.21;
-// // NOTE: KM INSERITI DALL'UTENTE
-// var richiestaKm = prompt("Inserisci i km che vuoi percorrere");
-// // ETA' INSERITA DALL'UTENTE
-// var anni = prompt("Inserisci la tua età");
-// // CALCOLO PREZZO INTERO
-// var prezzoIntero = richiestaKm * prezzoAlKm;
-// console.log(prezzoIntero);
-// // NOTE: ARROTONDAMENTO PREZZO
-// var prezzoArrotondato = prezzoIntero.toFixed(2);
-// // VARIABILE SCONTO
-// var sconto = 0;
-// // SCONTO UNDER18
-// if (anni < 18) {
-//   sconto = (prezzoIntero * 20 ) / 100;
-// // SCONTO OVER65
-// } else if (anni > 65) {
-//   sconto = (prezzoIntero * 40) / 100;
-// }
-// // PREZZO SCONTATO
-// var prezzoFinale = prezzoIntero - sconto;
-// console.log(prezzoFinale);
-//
-// document.getElementById("km").innerHTML = richiestaKm + " Km";
-//
-// document.getElementById("anni").innerHTML = anni + " Anni";
-//
-// document.getElementById("totale").innerHTML = prezzoArrotondato + " €";
