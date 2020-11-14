4c21d3136cdd5d7cf1da285e4e77163682db7e68
Alessandro <aleneri242@gmail.com>
2020-11-14 17:12:50 +0100

    commit 13 sabato

--- js/script.js
+++ js/script.js
@@ -14,37 +14,40 @@ function() {
 
   // NOTE: PREZZO AL KM
   var prezzoAlKm = 0.21;
-  // // NOTE: KM INSERITI DALL'UTENTE
-  // var richiestaKm = prompt("Inserisci i km che vuoi percorrere");
-  // ETA' INSERITA DALL'UTENTE
-  // var anni = prompt("Inserisci la tua età");
+
   // CALCOLO PREZZO INTERO
   var prezzoIntero = numeroKm * prezzoAlKm;
   console.log(prezzoIntero);
+
   // NOTE: ARROTONDAMENTO PREZZO
   var prezzoArrotondato = prezzoIntero.toFixed(2);
+
   // VARIABILE SCONTO
   var sconto = 0;
+
   // NOTE: MESSAGGIO
   var messaggio = "";
+
   // SCONTO UNDER18
   if (fasciaEta == "Minorenne") {
     sconto = (prezzoIntero * 20 ) / 100;
     messaggio = alert("Hai diritto al 20% di sconto!!");
+
   // SCONTO OVER65
 } else if (fasciaEta == "Over65") {
     sconto = (prezzoIntero * 40) / 100;
     messaggio = alert("Hai diritto al 40% di sconto!!");
   }
+
   // PREZZO SCONTATO
   var prezzoFinale = prezzoIntero - sconto;
   console.log(prezzoFinale);
 
-  // document.getElementById("km").innerHTML = richiestaKm + " Km";
-  //
-  // document.getElementById("anni").innerHTML = anni + " Anni";
-  //
-  // document.getElementById("totale").innerHTML = prezzoArrotondato + " €";
+  var nomeCognome = document.getElementById("nome_cognome").value;
+  document.getElementById("nomepasseggero").innerHTML = nomeCognome;
+
+
+
 
 
 
