300639af501770d42b9b321fb08f4dab0737f84b
Alessandro <aleneri242@gmail.com>
2020-11-15 13:48:28 +0100

    Aggiunta commenti css e js e fine esercizio

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ var bottoneGenera = document.getElementById("genera");
 bottoneGenera.addEventListener("click",
 function() {
 
+  // NOTE: RICHIESTA KM
   var numeroKm =
   document.getElementById("km").value;
   console.log(numeroKm);
 
+  // NOTE: RICHIESTA ETA'
   var fasciaEta = document.getElementById("fascia_eta").value;
   console.log(fasciaEta);
 
+  // NOTE: RICHIESTA NOME E COGNOME
   var nomeCognome = document.getElementById("nome_cognome").value;
   console.log(nomeCognome);
 
@@ -49,50 +52,57 @@ function() {
   var prezzoFinale = prezzoIntero - sconto;
   console.log(prezzoFinale);
 
+  // NOTE: STAMPA SU TICKET DI NOME E COGNOME
   var nomeCognome = document.getElementById("nome_cognome").value;
   document.getElementById("nomepasseggero").innerHTML = nomeCognome;
 
+  // NOTE: STAMPA SU TICKET NUMERO CARROZZA
   var numeroCarrozza = Math.floor(Math.random() * 9) + 1;
   document.getElementById("carrozza").innerHTML = numeroCarrozza;
 
+  // NOTE: STAMPA SU TICKET CODICE CP
   var codiceCp = Math.floor(Math.random() * 10000) + 90000;
   document.getElementById("codicecp").innerHTML = codiceCp;
 
+  // NOTE: STAMPA DU TICKET COSTO BIGLIETTO
   document.getElementById("costobiglietto").innerHTML = prezzoArrotondato + " €";
 
+  // NOTE: TITOLO "IL TUO BIGLIETTO"
   var h2Biglietto = document.getElementById('h2biglietto');
   h2Biglietto.classList.remove("hidden");
   h2Biglietto.classList.add("show");
 
-
+  // NOTE: TICKET
   var ilMioBiglietto = document.getElementById('ticket');
   ilMioBiglietto.classList.remove("hidden");
   ilMioBiglietto.classList.add("show");
   }
 );
 
-
+// NOTE: PULSANTE ANNULLA
 var bottoneAnnulla = document.getElementById("annulla");
 bottoneAnnulla.addEventListener("click",
 function() {
+  // NOTE: NASCONDI TICKET
   bottoneAnnulla = document.getElementById("ticket");
   bottoneAnnulla.classList.remove("show");
   bottoneAnnulla.classList.add("hidden");
 
+  // NOTE: NASCONDI TITOLO "IL TUO BIGLIETTO"
   var h2Biglietto = document.getElementById('h2biglietto');
   h2Biglietto.classList.remove("show");
   h2Biglietto.classList.add("hidden");
 
+// NOTE: SVUOTA CAMPI KM/ETA'/NOME-COGNOME
   document.getElementById("km").value = "";
   document.getElementById("fascia_eta").value = "";
   document.getElementById("nome_cognome").value = "";
 
+  // NOTE: SVUOTA CAMPI TICKET
   document.getElementById("nomepasseggero").innerHTML = "";
   document.getElementById("offerta").innerHTML = "";
   document.getElementById("carrozza").innerHTML = "";
   document.getElementById("codicecp").innerHTML = "";
   document.getElementById("costobiglietto").innerHTML = "";
-
-
 }
 );
